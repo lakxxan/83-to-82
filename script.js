0b09cb90a9ef7c8f94094e19fd6129cc20afbd19
Lakshan Rathnayake <95514525+lakxxan@users.noreply.github.com>
2025-03-31 18:10:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -103,49 +103,57 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
-  // ===== Enhanced Dual Image Transformation =====
-  const bgContainer = document.getElementById('home');
-  if (bgContainer) {
-    // Create image elements
-    const bgImage1 = document.createElement('div');
-    const bgImage2 = document.createElement('div');
-    
-    // Set classes for both images
-    bgImage1.className = 'absolute inset-0 bg-cover bg-center transition-opacity duration-1000 ease-in-out opacity-100 z-0';
-    bgImage2.className = 'absolute inset-0 bg-cover bg-center transition-opacity duration-1000 ease-in-out opacity-0 z-0';
-    
-    // Set your image URLs here
-    bgImage1.style.backgroundImage = "url('https://scontent-bom2-3.xx.fbcdn.net/v/t39.30808-6/471151805_122102167262659598_1827396236113203600_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeFsvno0lixBCnmNe_RyofA3Av2rn94DogAC_auf3gOiAHNzYgdfjVoQXuj5mdi2-_4v4BP32sDm0q1EPdCHecZ8&_nc_ohc=_V5VD7uIhzEQ7kNvgFg3ap3&_nc_oc=AdkepnGb6SXSoUhrzvx2FPaTCLqFLji2iNy80aR0y-dV_GRT-Kq0WfBjayG82NkBfoc&_nc_zt=23&_nc_ht=scontent-bom2-3.xx&_nc_gid=K34D5d78G44Ur_xjHVs-Sw&oh=00_AYEvhAv415wCCAIwjzG7cDytIsdgLKad4OyLG7zh6bQkmQ&oe=67F05157')";
-    bgImage2.style.backgroundImage = "url('https://scontent-bom1-1.xx.fbcdn.net/v/t39.30808-6/470811574_122102167610659598_5425146254650307480_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeEzdli0XX-yynyNQxzSlBw_X4GOw36Pn9FfgY7Dfo-f0f7xa1DpgXqaloGSaT3l2VtIkBlUEL22zL0eu8ZYlsRG&_nc_ohc=h-aHgzQJEXAQ7kNvgHOi6nL&_nc_oc=Adma0eoNjkWtbKRNDT66GdDEz683200VtKbOPJuvjw1gohXzcCazmfkGr-fVIHTa104&_nc_zt=23&_nc_ht=scontent-bom1-1.xx&_nc_gid=W57zLGHoLM_zVkf34-c5YQ&oh=00_AYHwUcscOIQW_uLOhciInBTxymvQUSS4o76yOSZckygXwg&oe=67F05979')";
-    
-    // Insert images at the beginning of the section
-    bgContainer.insertBefore(bgImage2, bgContainer.firstChild);
-    bgContainer.insertBefore(bgImage1, bgContainer.firstChild);
-    
-    // Image transition logic
-    let currentActive = 1;
-    const transitionImages = () => {
-      if (currentActive === 1) {
-        bgImage1.classList.remove('opacity-100');
-        bgImage1.classList.add('opacity-0');
-        bgImage2.classList.remove('opacity-0');
-        bgImage2.classList.add('opacity-100');
-        currentActive = 2;
-      } else {
-        bgImage1.classList.remove('opacity-0');
-        bgImage1.classList.add('opacity-100');
-        bgImage2.classList.remove('opacity-100');
-        bgImage2.classList.add('opacity-0');
-        currentActive = 1;
-      }
-    };
-    
-    // Start transition after 3 seconds, then every 5 seconds
-    setTimeout(() => {
-      transitionImages();
-      setInterval(transitionImages, 5000);
-    }, 3000);
-  }
+// ===== Enhanced Dual Image Transformation =====
+const bgContainer = document.getElementById('home');
+if (bgContainer) {
+  // Create image elements
+  const bgImage1 = document.createElement('div');
+  const bgImage2 = document.createElement('div');
+  
+  // Set classes for both images
+  bgImage1.className = 'absolute inset-0 bg-cover bg-center transition-opacity duration-1000 ease-in-out opacity-100 z-0';
+  bgImage2.className = 'absolute inset-0 bg-cover bg-center transition-opacity duration-1000 ease-in-out opacity-0 z-0';
+  
+  // Set your Facebook image URLs
+  bgImage1.style.backgroundImage = "url('https://scontent-bom1-1.xx.fbcdn.net/v/t39.30808-6/470811574_122102167610659598_5425146254650307480_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeEzdli0XX-yynyNQxzSlBw_X4GOw36Pn9FfgY7Dfo-f0f7xa1DpgXqaloGSaT3l2VtIkBlUEL22zL0eu8ZYlsRG&_nc_ohc=h-aHgzQJEXAQ7kNvgHOi6nL&_nc_oc=Adma0eoNjkWtbKRNDT66GdDEz683200VtKbOPJuvjw1gohXzcCazmfkGr-fVIHTa104&_nc_zt=23&_nc_ht=scontent-bom1-1.xx&_nc_gid=W57zLGHoLM_zVkf34-c5YQ&oh=00_AYHwUcscOIQW_uLOhciInBTxymvQUSS4o76yOSZckygXwg&oe=67F05979')";
+  bgImage2.style.backgroundImage = "url('https://scontent-bom2-3.xx.fbcdn.net/v/t39.30808-6/471151805_122102167262659598_1827396236113203600_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=f727a1&_nc_eui2=AeFsvno0lixBCnmNe_RyofA3Av2rn94DogAC_auf3gOiAHNzYgdfjVoQXuj5mdi2-_4v4BP32sDm0q1EPdCHecZ8&_nc_ohc=_V5VD7uIhzEQ7kNvgFg3ap3&_nc_oc=AdkepnGb6SXSoUhrzvx2FPaTCLqFLji2iNy80aR0y-dV_GRT-Kq0WfBjayG82NkBfoc&_nc_zt=23&_nc_ht=scontent-bom2-3.xx&_nc_gid=K34D5d78G44Ur_xjHVs-Sw&oh=00_AYEvhAv415wCCAIwjzG7cDytIsdgLKad4OyLG7zh6bQkmQ&oe=67F05157')";
+  
+  // Preload images to avoid flickering
+  const preloadImage = (url) => {
+    const img = new Image();
+    img.src = url;
+  };
+  preloadImage(bgImage1.style.backgroundImage.slice(4, -1).replace(/["']/g, ""));
+  preloadImage(bgImage2.style.backgroundImage.slice(4, -1).replace(/["']/g, ""));
+  
+  // Insert images at the beginning of the section
+  bgContainer.insertBefore(bgImage2, bgContainer.firstChild);
+  bgContainer.insertBefore(bgImage1, bgContainer.firstChild);
+  
+  // Image transition logic
+  let currentActive = 1;
+  const transitionImages = () => {
+    if (currentActive === 1) {
+      bgImage1.classList.remove('opacity-100');
+      bgImage1.classList.add('opacity-0');
+      bgImage2.classList.remove('opacity-0');
+      bgImage2.classList.add('opacity-100');
+      currentActive = 2;
+    } else {
+      bgImage1.classList.remove('opacity-0');
+      bgImage1.classList.add('opacity-100');
+      bgImage2.classList.remove('opacity-100');
+      bgImage2.classList.add('opacity-0');
+      currentActive = 1;
+    }
+  };
+  
+  // Start transition after 3 seconds, then every 5 seconds
+  setTimeout(() => {
+    transitionImages(); // First transition
+    setInterval(transitionImages, 5000); // Subsequent transitions
+  }, 3000);
+}
 
   // ===== Scroll Animations =====
   const animateOnScroll = function() {
